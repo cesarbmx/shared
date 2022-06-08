904fd8a4807730a786523faca8cf061e2ad419f6
Cesar C <cesarbmx@hotmail.com>
2022-06-08 17:54:33 +0200

    addinf comments

--- CesarBmx.Shared.Api/ActionFilters/StoreRequestInContext.cs
+++ CesarBmx.Shared.Api/ActionFilters/StoreRequestInContext.cs
@@ -4,6 +4,10 @@ namespace CesarBmx.Shared.Api.ActionFilters
 {
     public class StoreRequestInContextAttribute : IActionFilter
     {
+        /// <summary>
+        /// We store the request in the context so that we can grab it in case of an exception and properly log it.
+        /// </summary>
+        /// <param name="filterContext"></param>
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             if (filterContext.HttpContext.Request.Method == "POST")
@@ -12,7 +16,6 @@ namespace CesarBmx.Shared.Api.ActionFilters
                 filterContext.HttpContext.Items["Request"] = request;
             }
         }
-
         public void OnActionExecuted(ActionExecutedContext context)
         {
         }

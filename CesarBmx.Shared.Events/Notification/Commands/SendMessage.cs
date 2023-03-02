f71c40ae2e37523127e388fe72f8c2a79fe6fc06
Cesar C <cesarbmx@hotmail.com>
2023-03-02 11:49:27 +0100

    push

---
+++ CesarBmx.Shared.Events/Notification/Commands/SendMessage.cs
@@ -0,0 +1,11 @@
+﻿using CesarBmx.Shared.Messaging.CryptoWatcher.Types;
+
+namespace CesarBmx.Shared.Messaging.Notification.Commands
+{
+    #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public class SendMessage
+    {
+        public Guid MessageId { get; set; }
+
+    }
+}

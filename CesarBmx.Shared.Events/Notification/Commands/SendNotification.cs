d68e09796c2ea69866e955b939f700cc16884fc0
Cesar C <cesarbmx@hotmail.com>
2023-09-14 16:49:38 +0200

    phoneMessage and Email

--- CesarBmx.Shared.Events/Notification/Commands/SendNotification.cs
+++ CesarBmx.Shared.Events/Notification/Commands/SendNotification.cs
@@ -7,10 +7,9 @@ namespace CesarBmx.Shared.Messaging.Notification.Commands
     public class SendNotification
     {
         public Guid NotificationId { get; set; } = Guid.Empty;
-        public NotificationType NotificationType { get; set; } = NotificationType.TELEGRAM;
         public string UserId { get; set; } = null!;
-        public string PhoneNumber { get; set; } = null!;
         public string Text { get; set; } = null!;
+        public DateTime? ScheduledFor { get; set; } = null!;
 
     }
 }

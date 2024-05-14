f682c43a257a920a4b5d552bf1cf7635ba0d85c4
cesarc <cesarc@pinnacle.com>
2024-05-14 20:13:02 +0100

    add tag OTEL

--- CesarBmx.Shared.Messages/Ordering/Commands/CancelOrder.cs
+++ CesarBmx.Shared.Messages/Ordering/Commands/CancelOrder.cs
@@ -5,5 +5,6 @@ namespace CesarBmx.Shared.Messaging.Ordering.Commands
     public class CancelOrder
     {
         public Guid OrderId { get; set; }
+        public string UserId { get; set; } = null!;
     }
 }

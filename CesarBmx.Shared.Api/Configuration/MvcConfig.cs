f076e0aa97c61c52694abb99d7f1e90e5b9c9f83
cesarc <cesarc@pinnacle.com>
2024-05-28 21:01:39 +0200

    ssl

--- CesarBmx.Shared.Api/Configuration/MvcConfig.cs
+++ CesarBmx.Shared.Api/Configuration/MvcConfig.cs
@@ -68,6 +68,7 @@ namespace CesarBmx.Shared.Api.Configuration
                 if (enableRazorPages) endpoints.MapRazorPages();
                 endpoints.MapControllers();
             });
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             return app;

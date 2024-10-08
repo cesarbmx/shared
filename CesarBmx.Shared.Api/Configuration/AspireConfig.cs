66cafa2a6de95e17ae303a790c4e690c4bca8be6
cesarc <cesarc@pinnacle.com>
2024-10-08 16:04:44 +0200

    aspire

---
+++ CesarBmx.Shared.Api/Configuration/AspireConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+
+
+namespace CesarBmx.Shared.Api.Configuration
+{
+    public static class AspireConfig
+    {
+        public static void ConfigureSharedAspire(this IHostApplicationBuilder builder)
+        {
+            builder.AddSqlServerClient("SqlServer");
+        }
+    }
+}

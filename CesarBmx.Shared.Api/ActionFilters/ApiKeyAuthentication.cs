6437fe4a364400f521671c54a24d4717ec356bf6
cesarc <cesarc@pinnacle.com>
2025-03-17 13:55:03 +0100

    ApiKeyAuthorizationAttribute

---
+++ CesarBmx.Shared.Api/ActionFilters/ApiKeyAuthentication.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using CesarBmx.Shared.Settings;
+using CesarBmx.Shared.Application.Responses;
+using ErrorMessage = CesarBmx.Shared.Application.Messages.ErrorMessage;
+using System;
+using System.Linq;
+
+namespace CesarBmx.Shared.Api.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class ApiKeyAuthorizationAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly AuthenticationSettings _authenticationSettings;
+
+        public ApiKeyAuthorizationAttribute(AuthenticationSettings authenticationSettings)
+        {
+            _authenticationSettings = authenticationSettings;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var apiKey = context.HttpContext.Request.Headers["X-ApiKey"].FirstOrDefault();
+
+            if (string.IsNullOrEmpty(apiKey) || apiKey != _authenticationSettings.ApiKey)
+            {
+                var errorResponse = new Unauthorized(ErrorMessage.Unauthorized);
+                context.Result = new UnauthorizedObjectResult(errorResponse);
+            }
+        }
+    }
+}

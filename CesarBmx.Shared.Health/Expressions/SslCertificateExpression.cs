98ebc8de6dd368e98b4162f67956dc02ea4a6e8d
cesarc <cesarbmx@hotmail.com>
2026-04-23 14:25:45 +0200

    Avoid Compile

--- CesarBmx.Shared.Health/Expressions/SslCertificateExpression.cs
+++ CesarBmx.Shared.Health/Expressions/SslCertificateExpression.cs
@@ -6,31 +6,31 @@ namespace CesarBmx.Shared.Health.Expressions
 {
     public static class SslCertificateExpression
     {
-        public static Expression<Func<SslCertificate, bool>> CertificateMissing()
+        public static Func<SslCertificate, bool> CertificateMissing()
         {
             return x => !x.HasCertificate;
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateAboutToExpire(int daysToExpire)
+        public static Func<SslCertificate, bool> CertificateAboutToExpire(int daysToExpire)
         {
             return x => x.NotAfter.Date < DateTime.UtcNow.AddDays(daysToExpire).Date;
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateAlreadyExpired()
+        public static Func<SslCertificate, bool> CertificateAlreadyExpired()
         {
             return x => x.NotAfter.Date <= DateTime.UtcNow.Date;
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateIsNotValidYet()
+        public static Func<SslCertificate, bool> CertificateIsNotValidYet()
         {
             return x => x.NotBefore.Date > DateTime.UtcNow.Date;
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateHasNoIssuer()
+        public static Func<SslCertificate, bool> CertificateHasNoIssuer()
         {
             return x => string.IsNullOrEmpty(x.Issuer);
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateHasNoSubject()
+        public static Func<SslCertificate, bool> CertificateHasNoSubject()
         {
             return x => string.IsNullOrEmpty(x.Subject);
         }
-        public static Expression<Func<SslCertificate, bool>> CertificateDoesNotHaveVersionGreaterThan2()
+        public static Func<SslCertificate, bool> CertificateDoesNotHaveVersionGreaterThan2()
         {
             return x => x.Version < 3;
         }
